24d9443e0c63de667aeaaf833f6b5ef6a6b2e4b0
kmmehtamca <kapcareer@gmail.com>
2021-06-09 18:03:20 -0700

    Merge branch 'main' of https://github.com/kmmehtamca/Tic-Toe-Game into main

---
+++ Pizza Slicer.py
@@ -0,0 +1,28 @@
+
+print(
+    
+"""
+    Slicing 'Cheat Sheet'
+
+    0    1    2    3    4    5
+    +----+----+----+----+-----
+    | p  |  i | z  | z  | a  |
+    +----+----+----+----+-----
+    -5   -4  -3    -2  -1    0 
+"""
+)
+print("Enter the beginning and ending index for your slice of'Pizza'")
+print("Pres  the enter key at 'Start' to exit,")
+word = "pizza"
+start1 = None
+print(word[2:4])
+print("Kapil")
+while(start1 !="" ):
+    start1 = input("Enter the starting point")
+    if start1:
+        start1 = int(start1)
+        endpoint1= int(input("Enter the finish point"))
+        print("\n")
+        print(word[start1:endpoint1])
+
+print('\\Enter the enter key to exit')
